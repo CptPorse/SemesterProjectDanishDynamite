86eef5da4cd6744b77776c8b52999739cfce34b5
liekos <liekos@10.97.17.143>
2013-05-13 10:17:07 +0200

    Pis

--- Danish Crown Logistic System/src/gui/TrailerView.java
+++ Danish Crown Logistic System/src/gui/TrailerView.java
@@ -1,36 +1,177 @@
 package gui;
 
-import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
+import javax.swing.DefaultListModel;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.ListSelectionModel;
 import javax.swing.border.EmptyBorder;
 
-public class TrailerView extends JFrame {
+import model.Trailer;
+import dao.Dao;
+import dateutil.DU;
 
-	public TrailerView() {
+//Author: Soren Moller Nielsen
+public class TrailerView extends JFrame
+{
+
+	public TrailerView()
+	{
 		setResizable(false);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setBounds(1000, 100, 569, 347);
+		this.setBounds(1000, 100, 604, 347);
 		this.setTitle("Trailer View");
-		this.setVisible(true);
+
 		InitContent();
+		this.setVisible(true);
 
 	}
 
+	private Controller controller;
 	private JPanel contentPane;
 	private JLabel lblTrailerView;
+	private JTextField txfClock;
+	private JList<Trailer> lstArrivingTrailers;
+	private JList<Trailer> lstDeparturingTrailers;
 
-	private void InitContent() {
+	private DefaultListModel<Trailer> lstArrivingTrailersModel;
+
+	private DefaultListModel<Trailer> lstDeparturingTrailersModel;
+
+	private JLabel lblNewLabel;
+	private JLabel lblDeparture;
+	private JButton btnHasArrived, btnCheckDeparture;
+
+	private void InitContent()
+	{
+		controller = new Controller();
 
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
 
 		lblTrailerView = new JLabel("Trailer View");
-		contentPane.add(lblTrailerView, BorderLayout.NORTH);
+		lblTrailerView.setBounds(5, 5, 553, 14);
+		contentPane.add(lblTrailerView);
+
+		lstArrivingTrailersModel = new DefaultListModel<Trailer>();
+		lstArrivingTrailers = new JList<Trailer>();
+		lstArrivingTrailers.setBounds(5, 55, 155, 233);
+		contentPane.add(lstArrivingTrailers);
+		lstArrivingTrailers
+				.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+
+		lstDeparturingTrailersModel = new DefaultListModel<Trailer>();
+		lstDeparturingTrailers = new JList<Trailer>();
+		lstDeparturingTrailers.setBounds(285, 55, 155, 233);
+		contentPane.add(lstDeparturingTrailers);
+		lstDeparturingTrailers
+				.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+
+		btnHasArrived = new JButton("hasArrived");
+		btnHasArrived.setBounds(170, 265, 89, 23);
+		contentPane.add(btnHasArrived);
+		btnHasArrived.addActionListener(controller);
+
+		txfClock = new JTextField();
+		txfClock.setEditable(false);
+		txfClock.setBounds(175, 74, 86, 20);
+		contentPane.add(txfClock);
+		txfClock.setColumns(10);
+
+		btnCheckDeparture = new JButton("Check Departure");
+		btnCheckDeparture.setBounds(464, 265, 113, 23);
+		contentPane.add(btnCheckDeparture);
+		btnCheckDeparture.addActionListener(controller);
+
+		lblNewLabel = new JLabel("Arrivingtrailers");
+		lblNewLabel.setBounds(5, 30, 89, 14);
+		contentPane.add(lblNewLabel);
+
+		lblDeparture = new JLabel("Trailer ready for departure");
+		lblDeparture.setBounds(285, 30, 134, 14);
+		contentPane.add(lblDeparture);
+
+		controller.fillArrivingLst();
+		controller.fillDepartureLst();
 	}
 
+	public class Controller implements ActionListener
+	{
+		@Override
+		public void actionPerformed(ActionEvent e)
+		{
+			if (e.getSource() == btnHasArrived) {
+				if (lstArrivingTrailers.isSelectionEmpty() == true) {
+					System.out
+							.println("Tag dig sammen, du mangler at vælge et object fra listen");
+
+				} else {
+					(lstArrivingTrailers.getSelectedValue())
+							.setHasArrived(true);
+
+					controller.fillArrivingLst();
+
+				}
+			}
+			if (e.getSource() == btnCheckDeparture) {
+				if (lstDeparturingTrailers.isSelectionEmpty() == true) {
+
+					System.out
+							.println("Tag dig sammen, du mangler at vælge et object fra listen");
+
+				} else {
+
+					(lstArrivingTrailers.getSelectedValue())
+							.setTimeOfDeparture(DU.createDate());
+
+					controller.fillDepartureLst();
+
+				}
+			}
+		}
+
+		public void fillArrivingLst()
+		{
+			if (Dao.getTrailer().size() > 0) {
+
+				ArrayList<Trailer> arraylistTrailer = new ArrayList<>();
+				for (Trailer t : Dao.getTrailer()) {
+					if (t.hasArrived() == false) {
+						arraylistTrailer.add(t);
+					}
+				}
+				Trailer[] arrayTrailer = arraylistTrailer
+						.toArray(new Trailer[0]);
+
+				lstArrivingTrailers.setListData(arrayTrailer);
+			}
+		}
+
+		public void fillDepartureLst()
+		{
+			if (Dao.getTrailer().size() > 0) {
+
+				ArrayList<Trailer> arraylistTrailer = new ArrayList<>();
+				for (Trailer t : Dao.getTrailer()) {
+					if (t.isLoaded() == true && t.getTimeOfDeparture() == null) {
+						arraylistTrailer.add(t);
+					}
+				}
+				Trailer[] arrayTrailer = arraylistTrailer
+						.toArray(new Trailer[0]);
+
+				lstDeparturingTrailers.setListData(arrayTrailer);
+			}
+		}
+
+	}
 }
--- Danish Crown Logistic System/src/model/Trailer.java
+++ Danish Crown Logistic System/src/model/Trailer.java
@@ -7,11 +7,12 @@ import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.eclipse.jdt.annotation.Nullable;
 
 @NonNullByDefault
-public class Trailer {
+public class Trailer
+{
 
 	private String trailerID;
 	private Date timeOfArrival;
-	private Date timeOfDeparture;
+	private Date timeOfDeparture = null;
 	private boolean isLoaded = false;
 	private boolean hasArrived = false;
 	private double weightCurrent;
@@ -20,105 +21,128 @@ public class Trailer {
 	private Driver driver;
 	private ArrayList<ProductType> productTypes = new ArrayList<ProductType>();
 
-	public Trailer(String trailerID, double weightMax, Date timeOfArrival) {
+	public Trailer(String trailerID, double weightMax, Date timeOfArrival)
+	{
 		super();
 		this.timeOfArrival = timeOfArrival;
 		this.trailerID = trailerID;
 		this.weightMax = weightMax;
 	}
 
-	public String getTrailerID() {
+	public String getTrailerID()
+	{
 		return trailerID;
 	}
 
-	public void setTrailerID(String trailerID) {
+	public void setTrailerID(String trailerID)
+	{
 		this.trailerID = trailerID;
 	}
 
-	public Date getTimeOfArrival() {
+	public Date getTimeOfArrival()
+	{
 		return timeOfArrival;
 	}
 
-	public void setTimeOfArrival(Date timeOfArrival) {
+	public void setTimeOfArrival(Date timeOfArrival)
+	{
 		this.timeOfArrival = timeOfArrival;
 	}
 
-	public Date getTimeOfDeparture() {
+	@Nullable
+	public Date getTimeOfDeparture()
+	{
 		return timeOfDeparture;
 	}
 
-	public void setTimeOfDeparture(Date timeOfDeparture) {
+	public void setTimeOfDeparture(Date timeOfDeparture)
+	{
 		this.timeOfDeparture = timeOfDeparture;
 	}
 
-	public boolean isLoaded() {
+	public boolean isLoaded()
+	{
 		return isLoaded;
 	}
 
-	public void setLoaded(boolean isLoaded) {
+	public void setLoaded(boolean isLoaded)
+	{
 		this.isLoaded = isLoaded;
 	}
 
-	public boolean hasArrived() {
+	public boolean hasArrived()
+	{
 		return hasArrived;
 	}
 
-	public void setHasArrived(boolean hasArrived) {
+	public void setHasArrived(boolean hasArrived)
+	{
 		this.hasArrived = hasArrived;
 	}
 
-	public double getWeightCurrent() {
+	public double getWeightCurrent()
+	{
 		return weightCurrent;
 	}
 
-	public void setWeightCurrent(double weightCurrent) {
+	public void setWeightCurrent(double weightCurrent)
+	{
 		this.weightCurrent = weightCurrent;
 	}
 
-	public double getWeightMax() {
+	public double getWeightMax()
+	{
 		return weightMax;
 	}
 
-	public void setWeightMax(double weightMax) {
+	public void setWeightMax(double weightMax)
+	{
 		this.weightMax = weightMax;
 	}
 
 	@Nullable
-	public Driver getDriver() {
+	public Driver getDriver()
+	{
 		return driver;
 	}
 
-	public void setDriver(Driver driver) {
+	public void setDriver(Driver driver)
+	{
 		this.driver = driver;
 	}
 
-	public void clearDriver() {
+	public void clearDriver()
+	{
 		this.driver = null;
 	}
 
 	/**
 	 * Returns a list of productTypes in this Trailer.
 	 */
-	public ArrayList<ProductType> getProductTypes() {
+	public ArrayList<ProductType> getProductTypes()
+	{
 		return new ArrayList<ProductType>(productTypes);
 	}
 
 	/**
 	 * Adds the productType to this Trailer.
 	 */
-	public void addProductType(ProductType productType) {
+	public void addProductType(ProductType productType)
+	{
 		productTypes.add(productType);
 	}
 
 	/**
 	 * Removes the productType from this Trailer.
 	 */
-	public void removeProductType(ProductType productType) {
+	public void removeProductType(ProductType productType)
+	{
 		productTypes.remove(productType);
 	}
 
 	@Override
-	public String toString() {
+	public String toString()
+	{
 		return "Trailer " + trailerID;
 	}
 
