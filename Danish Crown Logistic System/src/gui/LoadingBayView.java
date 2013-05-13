a830aee93223f4e54e9f267298932ca67fba7173
liekos <liekos@10.97.17.143>
2013-05-13 12:52:05 +0200

    der vi kom til

--- Danish Crown Logistic System/src/gui/LoadingBayView.java
+++ Danish Crown Logistic System/src/gui/LoadingBayView.java
@@ -2,16 +2,18 @@ package gui;
 
 import java.awt.Font;
 
-import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.JTable;
+import javax.swing.ListSelectionModel;
 import javax.swing.border.EmptyBorder;
 
-public class LoadingBayView extends JFrame {
+public class LoadingBayView extends JFrame
+{
 
-	public LoadingBayView() {
+	public LoadingBayView()
+	{
 		setResizable(false);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setBounds(600, 500, 569, 347);
@@ -22,32 +24,29 @@ public class LoadingBayView extends JFrame {
 
 	private JPanel contentPane;
 	private JLabel lblTime, lbLoadingBay;
-	private JComboBox comboBox;
-	private JList lstLoadingInfo;
+	private JTable table;
 
-	private void InitContent() {
+	private void InitContent()
+	{
 
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 
-		comboBox = new JComboBox();
-		comboBox.setBounds(175, 30, 178, 20);
-		contentPane.add(comboBox);
-
-		lstLoadingInfo = new JList();
-		lstLoadingInfo.setBounds(30, 65, 503, 230);
-		contentPane.add(lstLoadingInfo);
-
 		lblTime = new JLabel("09:00");
 		lblTime.setFont(new Font("Tahoma", Font.BOLD, 16));
 		lblTime.setBounds(485, 30, 68, 17);
 		contentPane.add(lblTime);
 
-		lbLoadingBay = new JLabel("Loading Bay");
+		lbLoadingBay = new JLabel("Loading Bay view");
 		lbLoadingBay.setFont(new Font("Tahoma", Font.BOLD, 16));
-		lbLoadingBay.setBounds(211, 11, 99, 14);
+		lbLoadingBay.setBounds(211, 11, 164, 14);
 		contentPane.add(lbLoadingBay);
+
+		table = new JTable();
+		table.setBounds(47, 77, 506, 231);
+		contentPane.add(table);
+		table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 	}
 }
