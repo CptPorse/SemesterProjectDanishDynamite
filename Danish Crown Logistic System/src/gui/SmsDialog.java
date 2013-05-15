6986d1e9635e894e39fda531bf4fc2d90c5b7daf
Christian <Christian@10.97.20.26>
2013-05-15 11:18:53 +0200

    SMS done

--- Danish Crown Logistic System/src/gui/SmsDialog.java
+++ Danish Crown Logistic System/src/gui/SmsDialog.java
@@ -1,58 +1,97 @@
 package gui;
 
-import java.awt.BorderLayout;
-import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
-import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 import javax.swing.border.EmptyBorder;
 
-public class SmsDialog extends JDialog
+import model.Driver;
+import model.LoadingInfo;
+import model.Trailer;
+
+//Author Christian "sexy" Møller Pedersen
+public class SmsDialog extends JFrame
 {
+	private Controller controller;
 
-	private final JPanel contentPanel = new JPanel();
-
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args)
+	public SmsDialog(LoadingInfo loadingInfo)
 	{
-		try {
-			SmsDialog dialog = new SmsDialog();
-			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
-			dialog.setVisible(true);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		setResizable(false);
+		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		this.setBounds(600, 375, 350, 300);
+		this.setTitle("Sexy SMS 1.0");
+		InitContent(loadingInfo);
+		this.setVisible(true);
 	}
 
-	/**
-	 * Create the dialog.
-	 */
-	public SmsDialog()
+	private JPanel contentPane;
+	private JLabel lblReceiver, lblMessage;
+	private JTextArea txaReceiver, txaMessage;
+	private JButton btnOk;
+
+	private void InitContent(LoadingInfo loadingInfo)
 	{
-		setBounds(100, 100, 450, 300);
-		getContentPane().setLayout(new BorderLayout());
-		contentPanel.setLayout(new FlowLayout());
-		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
-		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		controller = new Controller();
+
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+
+		lblReceiver = new JLabel("Receiver:");
+		lblReceiver.setBounds(5, 5, 60, 24);
+		contentPane.add(lblReceiver);
+
+		txaReceiver = new JTextArea();
+		txaReceiver.setBounds(70, 5, 270, 24);
+		txaReceiver.setEditable(false);
+		contentPane.add(txaReceiver);
+
+		lblMessage = new JLabel("Message:");
+		lblMessage.setBounds(5, 44, 60, 24);
+		contentPane.add(lblMessage);
+
+		txaMessage = new JTextArea();
+		txaMessage.setBounds(5, 73, 335, 160);
+		txaMessage.setEditable(false);
+		contentPane.add(txaMessage);
+
+		btnOk = new JButton("OK");
+		btnOk.setBounds(145, 240, 60, 25);
+		btnOk.addActionListener(controller);
+		contentPane.add(btnOk);
+
+		//Receiver
+		Driver driver = loadingInfo.getSubOrder().getTrailer().getDriver();
+		if (driver != null)
 		{
-			JPanel buttonPane = new JPanel();
-			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
-			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			Trailer trailer = driver.getTrailer();
+			if (trailer != null)
 			{
-				JButton okButton = new JButton("OK");
-				okButton.setActionCommand("OK");
-				buttonPane.add(okButton);
-				getRootPane().setDefaultButton(okButton);
+				txaReceiver.setText(driver.getName() + " (" + driver.getPhoneNumber() + ")");
+				txaMessage.setText("Hi " + driver.getName() + ".\n\rYour trailer (" + trailer.getTrailerID()
+						+ ") is now finished loading.\n\rPlease pick it up at loading bay " + loadingInfo.getLoadingBay().getLoadingBayNumber()
+						+ ".\n\rRegards\n\rDanish Crown");
 			}
+		}
+
+	}
+
+	private class Controller implements ActionListener
+	{
+
+		@Override
+		public void actionPerformed(ActionEvent arg0)
+		{
+			if (arg0.getSource() == btnOk)
 			{
-				JButton cancelButton = new JButton("Cancel");
-				cancelButton.setActionCommand("Cancel");
-				buttonPane.add(cancelButton);
+				((JFrame)btnOk.getTopLevelAncestor()).dispose();
 			}
 		}
 	}
-
 }
--- Danish Crown Logistic System/src/gui/SystemStartUp.java
+++ Danish Crown Logistic System/src/gui/SystemStartUp.java
@@ -4,13 +4,15 @@ import service.Service;
 import service.StartUpService;
 
 //Author: Jens Nyberg Porse
-public class SystemStartUp {
+public class SystemStartUp
+{
 
 	private static ExternalSystemView externalSystemView;
 	private static TrailerView trailerView;
 	private static LoadingBayView loadingBayView;
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 
 		StartUpService.startUpData();
 		Service.sortTrailerArrival();
@@ -19,6 +21,5 @@ public class SystemStartUp {
 		trailerView = new TrailerView();
 		loadingBayView = new LoadingBayView();
 		externalSystemView = new ExternalSystemView();
-
 	}
 }
