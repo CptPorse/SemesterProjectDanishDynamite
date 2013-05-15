d99acfa91fdbd20f7fc5cc1a27c7e0c990ec7384
Christian <Christian@10.97.20.26>
2013-05-15 09:42:07 +0200

    Cleaned LoadingInfo color codes.

--- Danish Crown Logistic System/src/model/LoadingInfo.java
+++ Danish Crown Logistic System/src/model/LoadingInfo.java
@@ -3,7 +3,8 @@ package model;
 import java.util.Date;
 
 //Author: Jens Nyberg Porse
-public class LoadingInfo {
+public class LoadingInfo
+{
 
 	private Date timeOfLoadingStart;
 	private Date timeOfLoadingEnd;
@@ -12,7 +13,8 @@ public class LoadingInfo {
 	private LoadingBay loadingBay;
 	private LoadingInfoState state;
 
-	public LoadingInfo(SubOrder subOrder, LoadingBay loadingBay) {
+	public LoadingInfo(SubOrder subOrder, LoadingBay loadingBay)
+	{
 		super();
 		this.subOrder = subOrder;
 		this.loadingBay = loadingBay;
@@ -20,50 +22,61 @@ public class LoadingInfo {
 		state = LoadingInfoState.PENDING;
 	}
 
-	public Date getTimeOfLoadingStart() {
+	public Date getTimeOfLoadingStart()
+	{
 		return timeOfLoadingStart;
 	}
 
-	public void setTimeOfLoadingStart(Date timeOfLoadingStart) {
+	public void setTimeOfLoadingStart(Date timeOfLoadingStart)
+	{
 		this.timeOfLoadingStart = timeOfLoadingStart;
 	}
 
-	public Date getTimeOfLoadingEnd() {
+	public Date getTimeOfLoadingEnd()
+	{
 		return timeOfLoadingEnd;
 	}
 
-	public void setTimeOfLoadingEnd(Date timeOfLoadingEnd) {
+	public void setTimeOfLoadingEnd(Date timeOfLoadingEnd)
+	{
 		this.timeOfLoadingEnd = timeOfLoadingEnd;
 	}
 
-	public boolean isHighPriority() {
+	public boolean isHighPriority()
+	{
 		return highPriority;
 	}
 
-	public void setHighPriority(boolean highPriority) {
+	public void setHighPriority(boolean highPriority)
+	{
 		this.highPriority = highPriority;
 	}
 
-	public SubOrder getSubOrder() {
+	public SubOrder getSubOrder()
+	{
 		return subOrder;
 	}
 
-	public void setSubOrder(SubOrder subOrder) {
+	public void setSubOrder(SubOrder subOrder)
+	{
 		this.subOrder = subOrder;
 	}
 
-	public LoadingBay getLoadingBay() {
+	public LoadingBay getLoadingBay()
+	{
 		return loadingBay;
 	}
 
-	public void setLoadingBay(LoadingBay loadingBay) {
+	public void setLoadingBay(LoadingBay loadingBay)
+	{
 		this.loadingBay = loadingBay;
 	}
 
 	/**
 	 * @return the state
 	 */
-	public LoadingInfoState getState() {
+	public LoadingInfoState getState()
+	{
 		return state;
 	}
 
@@ -71,34 +84,37 @@ public class LoadingInfo {
 	 * @param state
 	 *            the state to set
 	 */
-	public void setState(LoadingInfoState state) {
+	public void setState(LoadingInfoState state)
+	{
 		this.state = state;
 	}
 
 	//Author: Christian Møller Pedersen
 	@Override
-	public String toString() {
+	public String toString()
+	{
 		String line = "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - ";
 		String bgColor = "";
-		if (state == LoadingInfoState.FINISHED) {
+		if (state == LoadingInfoState.FINISHED)
+		{
 			bgColor = "<body bgcolor='lime'>";
-		} else if (state == LoadingInfoState.READY_TO_LOAD) {
-			bgColor = "<body bgcolor='lightblue'>";
-		} else if (state == LoadingInfoState.LOADING) {
+		}
+		else if (state == LoadingInfoState.READY_TO_LOAD)
+		{
+			bgColor = "<body bgcolor='aqua'>";
+		}
+		else if (state == LoadingInfoState.LOADING)
+		{
 			bgColor = "<body bgcolor='yellow'>";
 		}
 
 		String hp = "";
-		if (highPriority == true) {
+		if (highPriority == true)
+		{
 			hp = "<h3 align='center'>HIGH PRIORITY</h3><br>";
 		}
-		return "<html>" + bgColor + hp
-				+ "<table><tr><td width='255'>Suborder: " + subOrder
-				+ "</td><td width='255' align='right'>State: " + state
-				+ "</td></tr><br><tr><td>Estimated start: "
-				+ timeOfLoadingStart
-				+ " </td><td align='right'>Estimated loading time: "
-				+ subOrder.getEstimatedLoadingTime() + " min</td></tr></table>"
-				+ line;
+		return "<html>" + bgColor + hp + "<table><tr><td width='255'>Suborder: " + subOrder + "</td><td width='255' align='right'>State: " + state
+				+ "</td></tr><br><tr><td>Estimated start: " + timeOfLoadingStart + " </td><td align='right'>Estimated loading time: "
+				+ subOrder.getEstimatedLoadingTime() + " min</td></tr></table>" + line;
 	}
 }
