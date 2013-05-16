78a71aa600befa2c981789568c3dbee047a7809c
Christian <Christian@10.97.20.201>
2013-05-16 10:22:45 +0200

    LoadingInfo States updated

--- Danish Crown Logistic System/src/model/LoadingInfoState.java
+++ Danish Crown Logistic System/src/model/LoadingInfoState.java
@@ -1,5 +1,13 @@
 package model;
 
-public enum LoadingInfoState {
+//Author: Christian Møller Pedersen
+/*
+ * PENDING: 		Trailer is on its way to DC.
+ * READY_TO_LOAD: 	Trailer has arrived at DC and is waiting to begin loading.
+ * LOADING: 		Trailer is currently being loaded.
+ * FINISHED: 		Trailer is loaded and leaving DC.
+ */
+public enum LoadingInfoState
+{
 	PENDING, READY_TO_LOAD, LOADING, FINISHED;
 }
